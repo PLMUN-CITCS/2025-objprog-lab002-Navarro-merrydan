f9709a095201d3ffaeec4ff4e236c6b1526040b0
Merry Dan Navarro <navarromerrydan_bscs@plmun.edu.ph>
2025-02-26 09:49:18 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

--- VariablesAndCaseSensitivity.java
+++ VariablesAndCaseSensitivity.java
@@ -1,8 +1,8 @@
 public class VariablesAndCaseSensitivity {
     public static void main(String[] args) {
-        int studentAge = 20;
-        double itemPrice = 09.99;
-        String studentName = "Merry Dan";
+        int studentAge = 15;
+        double itemPrice = 29.99;
+        String studentName = "Alice";
         int counter = 10;
         int Counter = 20;
 
